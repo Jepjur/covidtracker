e11e5386cdb40f8fbdb9e42883c40917745d160b
Jasper <Jasper_vandijk@hotmail.com>
2021-07-13 19:13:29 +0200

    fetched data from api

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,8 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async () => {
     try {
         const response = await axios.get(url);
-        console.log(response);
+        return response;
+        
     } catch (error) {
         
     }
