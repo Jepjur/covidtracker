17a23959fb84ce8ca46fb755b1e8df6e47da41ad
Jasper <Jasper_vandijk@hotmail.com>
2021-07-14 12:34:50 +0200

    added materialUI cards

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,14 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
+// Fetches data from the covid19 api
 export const fetchData = async () => {
     try {
-        const response = await axios.get(url);
-        return response;
-        
+        const { data: {confirmed, recovered, deaths, lastUpdate }} = await axios.get(url);
+
+        const modifiedData = { confirmed, recovered, deaths, lastUpdate, }
+        return modifiedData;
+
     } catch (error) {
         
     }
